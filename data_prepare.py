1acd80dbc6ada704551b73b962bad664a58fbbbe
hitlic <hitlic@users.noreply.github.com>
2019-05-04 12:24:23 +0800

    Update data_prepare.py

--- data_prepare.py
+++ data_prepare.py
@@ -31,40 +31,13 @@ class GNNGraph(object):
             node_tags: 节点标签整数列表
             node_features: 节点特征numpy数组
         '''
-        self.num_nodes = g.number_of_nodes() # ---------------- 节点数量
-        self.node_labels = node_tags         # ---------------- 边标签
-        self.label = label                   # ---------------- 图标签
-        self.node_features = node_features   # ---------------- 节点特征 numpy array (node_num * feature_dim)
+        self.num_nodes = g.number_of_nodes() # 节点数量
+        self.node_labels = node_tags         # 边标签
+        self.label = label                   # 图标签
+        self.node_features = node_features   # 节点特征 numpy array (node_num * feature_dim)
 
-        self.degrees = list(dict(g.degree()).values())   # ---------------- 节点的度列表
+        self.degrees = list(dict(g.degree()).values())   # 节点的度列表
         self.edges = list(g.edges)           # 网络边列表
-
-        ## 将边表示为numpy数组
-        #if g.number_of_edges() != 0:
-        #    x, y = zip(*g.edges())
-        #    self.num_edges = len(x)           # ---------------- 边数量
-        #    self.edge_pairs = np.ndarray(shape=(self.num_edges, 2), dtype=np.int32)
-        #    self.edge_pairs[:, 0] = x
-        #    self.edge_pairs[:, 1] = y
-        #    self.edge_pairs = self.edge_pairs.flatten()   # ---------------- 边对组
-        #else:
-        #    self.num_edges = 0
-        #    self.edge_pairs = np.array([])
-
-        # 处理图的边特征，表示为numpy数组
-        # self.edge_features = None
-        # if nx.get_edge_attributes(g, 'features'):
-        #     # make sure edges have an attribute 'features' (1 * feature_dim numpy array)
-        #     edge_features = nx.get_edge_attributes(g, 'features')
-        #     assert(type(edge_features.values()[0]) == np.ndarray)
-        #     # need to rearrange edge_features using the e2n edge order
-        #     edge_features = {(min(x, y), max(x, y)): z for (x, y), z in edge_features.items()}
-        #     keys = sorted(edge_features)
-        #     self.edge_features = []
-        #     for edge in keys:
-        #         self.edge_features.append(edge_features[edge])
-        #         self.edge_features.append(edge_features[edge])  # add reversed edges
-        #     self.edge_features = np.concatenate(self.edge_features, 0)  # ---------------- 边特征
 
 
 def load_data(data, test_number=0, fold=1):
